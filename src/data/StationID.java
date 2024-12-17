504d3129e92d145ae818ea39f846d3c201a01e15
Marc <124775478+Maarc89@users.noreply.github.com>
2024-12-17 17:53:03 +0100

    wip

--- src/data/StationID.java
+++ src/data/StationID.java
@@ -2,8 +2,13 @@ package data;
 
 public class StationID {
 
-    public void main() {
-        System.out.println("StationID class");
+    private final String stationID;
+
+    public StationID(String stationID) {
+        this.stationID = stationID;
     }
 
+    public String getStationID () {
+        return stationID;
+    }
 }
