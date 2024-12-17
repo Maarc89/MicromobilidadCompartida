5c62d9cfb539ae8b1b1ad7c7c70b51bbbfbee409
polete11jj <poljaimejuancaubet@gmail.com>
2024-12-17 17:47:38 +0100

    testGeographicPoint +-180 / +-90

--- src/data/GeographicPoint.java
+++ src/data/GeographicPoint.java
@@ -10,6 +10,14 @@ final public class GeographicPoint {
     private final float longitude;
 
     public GeographicPoint(float latitude, float longitude) {
+
+        if (latitude < -90.0f || latitude > 90.0f) {
+            throw new IllegalArgumentException("Latitude must be between -90.0 and 90.0 degrees.");
+        }
+        if (longitude < -180.0f || longitude > 180.0f) {
+            throw new IllegalArgumentException("Longitude must be between -180.0 and 180.0 degrees.");
+        }
+
         this.latitude = latitude;
         this.longitude = longitude;
     }
@@ -29,13 +37,6 @@ final public class GeographicPoint {
         if (o == null || getClass() != o.getClass()) return false;
         GeographicPoint gP = (GeographicPoint) o;
         eq = ((latitude == gP.latitude) && (longitude == gP.longitude));
-
-        System.out.println("winwin");
-        System.out.println("aaaa");
-        System.out.println("aaaaaa");
-
-        System.out.println("wef");
-
         return eq;
     }
 
