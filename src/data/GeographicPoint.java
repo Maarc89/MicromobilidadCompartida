17912615df7d0549a0212a29542364b0462b96d4
polete11jj <poljaimejuancaubet@gmail.com>
2024-12-17 17:04:59 +0100

    COMMIT5

--- src/data/GeographicPoint.java
+++ src/data/GeographicPoint.java
@@ -33,8 +33,10 @@ final public class GeographicPoint {
         eq = ((latitude == gP.latitude) && (longitude == gP.longitude));
 
         System.out.println("winwin");
-        System.out.println("winwin");
-        System.out.println("winwin");
+        System.out.println("h");
+        System.out.println("h");
+
+        System.out.println("h");
 
         return eq;
     }
