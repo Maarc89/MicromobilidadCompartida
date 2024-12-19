cd8ac9360ce65bb3f66d82b66c6b862af9b125e2
polete11jj <poljaimejuancaubet@gmail.com>
2024-12-19 15:17:10 +0100

    qr image

--- src/micromobility/JourneyRealizeHandler.java
+++ src/micromobility/JourneyRealizeHandler.java
@@ -3,15 +3,31 @@ package micromobility;
 import data.GeographicPoint;
 import data.StationID;
 import exceptions.*;
+import services.smartfeatures.QRDecoder;
+import services.smartfeatures.UnbondedBTSignal;
 
 import java.time.LocalDateTime;
+
+import java.awt.image.BufferedImage;
 
 public class JourneyRealizeHandler {
     // ???
 
+    private QRDecoder qrdecoder;
+    private UnbondedBTSignal bluetooth;
+    private BufferedImage img;
     // The class members
+    /*StationID stID;
+    GeographicPoint gP;
+    LocalDateTime date;
+    float distance;
+    int duration;
+    float averageSpeed;
+*/
+
 
     // The constructor/s
+    public
 
     // Different input events that intervene
 
@@ -19,6 +35,14 @@ public class JourneyRealizeHandler {
     public void scanQR() throws ConnectException, InvalidPairingArgsException,
             CorruptedImgException, PMVNotAvailException,
             ProceduralException {
+
+
+
+            qrdecoder.getVehicleID(img);
+            bluetooth.BTbroadcast();
+
+            System.out.println();
+            System.out.println();
     }
 
     public void unPairVehicle() throws ConnectException, InvalidPairingArgsException,
@@ -27,6 +51,7 @@ public class JourneyRealizeHandler {
 
     // Input events from the unbonded Bluetooth channel
     public void broadcastStationID(StationID stID) throws ConnectException {
+
     }
 
     // Input events from the Arduino microcontroller channel
@@ -40,7 +65,8 @@ public class JourneyRealizeHandler {
     private void calculateValues(GeographicPoint gP, LocalDateTime date) {
     }
 
-    private void calculateImport(float dis, int dur, float avSp, LocalDateTime date) {
+    private void calculateImport(float distance, int duration, float averageSpeed, LocalDateTime date) {
+
     }
 
     // Setter methods for injecting dependencies
