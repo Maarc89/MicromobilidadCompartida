4f99c7ac3decc03216bbcb30b3a741cd6f50bba7
Marc <124775478+Maarc89@users.noreply.github.com>
2024-12-29 16:33:18 +0100

    rename testsserver

--- test/services/TestsServer.java
+++ test/services/TestsServer.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MockServerTest {
+class TestsServer {
 
     private MockServer mockServer;
     private UserAccount user;
